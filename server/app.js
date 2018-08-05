ab94a7e2a27240d10951a902e7967698a15fd575
guillaumepn <guillaumepn@free.fr>
2018-08-05 23:55:39 +0200

    add CORS handle for Express app + fix some css

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect('mongodb://guillaumepn:test1234@ds113452.mlab.com:13452/gql-test', { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
